1cd7a2940369feb8f8208c6f32bf9e29376a37f8
Amir1235 <42092550+Amir1235@users.noreply.github.com>
2025-09-13 20:23:04 +0200

    new article object can now be created

--- src/components/ArticleEditor.vue
+++ src/components/ArticleEditor.vue
@@ -1,5 +1,29 @@
 <template>
 
+<input type="text" v-model="title" placeholder="Titel">
+<button @click="saveArticle">Save</button>
 </template>
+
 <script setup lang="ts">
+import {ref} from 'vue'
+import type { Article } from '../types/ccms'
+import { useArticleStore } from '../store/ccms'
+
+const store = useArticleStore()
+
+const title = ref("")
+
+
+
+function saveArticle(){
+    const newArticle: Article ={
+        id: crypto.randomUUID(),
+        title: title.value,
+        blockIds: [],
+        createdAt: new Date()
+        
+    }
+    store.addArticle(newArticle)
+    title.value =""
+}
 </script>
